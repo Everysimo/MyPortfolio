7c3eeefa1678cd92f882b786a30bcf9410bd8729
Everysimo <simone.spera.13@gmail.com>
2021-07-18 19:50:02 +0200

    Aggiunta cronologia qualifiche

--- assets/js/code.js
+++ assets/js/code.js
@@ -46,3 +46,23 @@ function toggleSkills(){
 skillsHeader.forEach((el) =>{
     el.addEventListener('click',toggleSkills)
 })
+
+/* ======= QUALIFICATION TABS ======= */
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
