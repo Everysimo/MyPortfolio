33241f24ae18388bdc4870283b12992b59f9a58e
Everysimo <simone.spera.13@gmail.com>
2021-07-18 13:03:49 +0200

    Added Skills list

--- assets/js/code.js
+++ assets/js/code.js
@@ -27,3 +27,22 @@ function linkAction(){
 }
 //apply the event onclick for each navLink
 navLink.forEach(n => n.addEventListener("click",linkAction))
+
+/* ======= ACCORDION SKILLS ======= */
+const skillsContent = document.getElementsByClassName('skills_content'),
+    skillsHeader = document.querySelectorAll('.skills_header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click',toggleSkills)
+})
